fb02b801127ac7ce1971b2d9d43c14ddf65ae88b
DeanHH <532171255@qq.com>
2014-07-16 22:40:38 +0800

    Add Test project for zeromq
    Add Test project for zeromq

---
+++ GNE/src/ZeromqTest/Src/ClientTest/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  ZeromqTest
+//
+//  Created by DeanHH on 14-7-16.
+//  Copyright (c) 2014年 DeanHH. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World client!\n";
+    return 0;
+}
+
